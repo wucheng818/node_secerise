478a4d9e588b9d5a8d4930456e6b82d69d0c102b
wucheng818 <orangewu818@163.com>
2019-11-06 14:40:59 +0800

    Feat 路由模块自动加载

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 const app = new Koa();
-const router = new Router();
+const Router = require('koa-router');
+const classic = require('./api/v1/classic');
+const book = require('./api/v1/book');
+const requireDirectory = require('require-directory');
+
+requireDirectory(module, './api', {
+  visit: whenLoadModule
+});
+function whenLoadModule(obj) {
+  if (obj instanceof Router) {
+    app.use(obj.routes());
+  }
+}
+// app.use(book.routes())
+// app.use(classic.routes())
+app.listen(3000);
